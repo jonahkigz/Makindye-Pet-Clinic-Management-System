0baec16f6c90493ab86c114c2dd3acd584a6c2e7
jonahkigz <37324434+jonahkigz@users.noreply.github.com>
2026-07-02 02:56:38 +0000

    Fix syntax error in Vite directive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MPCMS - Makindye Pet Clinic</title>
 
-   @vite(['resources/css/app.css', 'resources/js/app.js'], asset(''))
+   @vite(['resources/css/app.css', 'resources/js/app.js']
 </head>
 
 <body class="bg-gray-100 antialiased">
