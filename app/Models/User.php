17d6b3fdf00d7162eed369ed1887211ed680c032
jonahkigz <37324434+jonahkigz@users.noreply.github.com>
2026-07-06 09:54:53 +0300

    Add owner relationship to User model
    Added owner relationship method to User model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,15 +11,10 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-     use HasRoles;
+    use HasRoles;
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -27,26 +22,21 @@ class User extends Authenticatable
         'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function owner()
+    {
+        return $this->hasOne(Owner::class, 'user_id');
     }
 }
