4fdba4b65acc623055a42f5f13db0446616b9d66
jonahkigz <37324434+jonahkigz@users.noreply.github.com>
2026-07-07 10:51:35 +0000

    Fix species migration syntax

--- database/migrations/2026_07_07_095699_create_species_table.php
+++ database/migrations/2026_07_07_095699_create_species_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-   public function up(): void
-{
-    Schema::create('species', function (Blueprint $table) {
-        $table->id();
-        $table->string('name')->unique();
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('species', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+    }
 
-public function down(): void
-{
-    Schema::dropIfExists('species');
-}
+    public function down(): void
+    {
+        Schema::dropIfExists('species');
+    }
 };
